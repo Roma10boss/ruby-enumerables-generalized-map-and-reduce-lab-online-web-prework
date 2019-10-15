745121acffa4575004030e306a8267146a9696c5
Romario Gustave <romagustave10@gmail.com>
2019-10-15 06:38:48 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,15 @@
 # Your Code Here
+def map()
+  map = [1,2,3,4]
+end
+
+def map_2_negative()
+map_2_negative = map *(-1)
+end
+
+def reduce()
+  red = map[0]+map[1]+map[2]
+end
+def general 
+  puts map_2_negative
+   puts red
